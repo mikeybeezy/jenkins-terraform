2b81eeadb9c45b28440ff9223aeae2de6be705ab
Mike Babs <mikeybabs40@gmail.com>
2022-12-27 23:43:25 +0000

    updated jekins bootstarp script

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -11,6 +11,25 @@ java -version
 
 sudo apt upgrade -y
 
+## install Docker ##
+
+sudo apt install docker.io -y
+
+docker -version
+
+sudo systemctl start docker
+
+# sudo systemctl enable docker
+
+sudo systemctl status docker
+
+## install kubectl 
+sudo snap install kubectl  --classic
+
+kubectl version --client
+
+## instal Jenkins 
+
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
@@ -30,6 +49,12 @@ sudo systemctl status jenkins
 sudo ufw allow 8080
 
 sudo ufw status
+
+# sudo usermod -aG docker jenkins 
+
+sudo systemctl restart docker.service 
+
+sudo echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
 
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
